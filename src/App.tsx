8e4248aab638af89f89ec87026a63ba3c96cbb74
domingohernani <dev.hernani.domingo@gmail.com>
2025-10-28 21:30:38 +0800

    feat: Implement testing api llm functionality and theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,44 @@
 import { Button } from "@/components/ui/button";
-import { ThemeProvider } from "@/components/theme-provider";
-import { ModeToggle } from "./components/mode-toggle";
+import { ModeToggle } from "./components/ui/mode-toggle";
+import { useState } from "react";
+import axios from "axios";
+import { Input } from "./components/ui/input";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [response, setResponse] = useState("");
+
+  const handleBtnClick = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.post("https://apifreellm.com/api/chat", {
+        message: value,
+      });
+      setResponse(data.response);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <section>
       <ModeToggle />
-      <section className="h-screen flex items-center justify-center">
-        <Button variant={"outline"}>Record</Button>
-      </section>
-    </ThemeProvider>
+      <div>
+        <Input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <Button variant={"outline"} onClick={handleBtnClick}>
+          Send
+        </Button>
+      </div>
+
+      {loading ? "Loading..." : <p>{response}</p>}
+    </section>
   );
 }
 
